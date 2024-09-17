c2eeb62111d27ef5a8d60c5708881751fc265ee4
Hugo <Hugoabouda@gmail.com>
2024-09-17 18:10:41 +0200

    vbrubrve

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: habouda <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 01:08:09 by habouda           #+#    #+#             */
-/*   Updated: 2024/09/17 17:30:52 by habouda          ###   ########.fr       */
+/*   Updated: 2024/09/17 18:09:57 by habouda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,15 @@ void	execute(char *envp[], char *argv)
 {
 	char	**cmd;
 	char	*path;
+	int		i;
+	i = 0;
+	// ft_printf("erreur");
 
+	// access(/bin/ls, argv) == 0
+	while (ft_strnstr(envp[i], "PATH", 4) == 0)
+		i++;
 	cmd = ft_split(argv, ' ');
-	path = ft_strjoin("/usr/bin/", cmd[0]);
+	path = ft_strjoin("/bin/", cmd[0]);
 	execve(path, cmd, envp);
 }
 void	child_process(int fd[2], char *argv[], char **envp)
@@ -27,7 +33,7 @@ void	child_process(int fd[2], char *argv[], char **envp)
 	int	file;
 	
 	file = open(argv[1], O_RDONLY);
-	if (file == -1)
+	if (file < 0)
 		perror("cannot open file");
 	dup2(fd[1], STDOUT_FILENO);
 	dup2(file, STDIN_FILENO);
@@ -39,8 +45,8 @@ void	parent_process(int fd[2], char *argv[], char **envp)
 {
 	int	file;
 
-	file = open(argv[4], O_RDONLY);
-	if (file == -1)
+	file = open(argv[4], O_WRONLY);
+	if (file < 0)
 		perror("cannot open file");
 	dup2(file , STDOUT_FILENO);
 	dup2(fd[0], STDIN_FILENO);
